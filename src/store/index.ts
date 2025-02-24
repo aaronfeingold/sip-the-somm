b69fb8c3f70412c359da35cba0dd1385121ffa22
aaron.feingold <ajfeingold88@gmail.com>
2025-02-23 22:35:38 -0600

    feature(store): add middleware to use localStorage to retain state

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import chatReducer from "./chatSlice";
+import { localStorageMiddleware } from "./clientStorageMiddleware";
 
 export const store = configureStore({
   reducer: {
     chat: chatReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
   // Enable Redux DevTools in development
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== "production",
 });
 
 export type RootState = ReturnType<typeof store.getState>;
