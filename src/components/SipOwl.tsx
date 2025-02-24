d8cb2c95e4989560c032ba920e8688a65010a103
aaron.feingold <ajfeingold88@gmail.com>
2025-02-23 22:38:54 -0600

    fix(siptheowl): import relative to svg for sip

--- src/components/SipOwl.tsx
+++ src/components/SipOwl.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import sipTheOwl from "@/public/sipTheOwl.svg";
+import sipTheOwl from "../../public/sipTheOwl.svg";
 
 interface SipOwlProps {
   isExpanded?: boolean;
